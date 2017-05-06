b0922d0930380eec71c88b3b0f7e59401e947d32
Jacob Harasimo <jacobharasimo@gmail.com>
2017-05-05 22:56:31 -0400

    updates to style

--- src/components/flight-results/flight-results.module.ts
+++ src/components/flight-results/flight-results.module.ts
@@ -2,10 +2,11 @@ import * as angular from 'angular';
 import 'angular-ui-bootstrap';
 import {FlightResultsComponent} from './flight-results.component';
 import './flight-results.scss';
-
+import 'angular-sanitize';
 export const FlightResultsModule = angular.module('flightResults',
     [
-        'ui.bootstrap'
+        'ui.bootstrap',
+        'ngSanitize'
     ])
     .component('flightResults', FlightResultsComponent)
     .name;
